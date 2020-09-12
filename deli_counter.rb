b7279a0dae6f4b12b2a8740b9411d347e9271dc7
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 22:54:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def take_a_number(katz_deli, name = "Ada")
   puts "Welcome, #{name}. You are number #{number} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if katz_deli.length < 1
     puts "There is nobody waiting to be served!"
   else
