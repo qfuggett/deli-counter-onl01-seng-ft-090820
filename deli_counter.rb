402e95f1a59cb8c053695305947b15a99372490c
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-12 22:33:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name = "Ada")
+def take_a_number(katz_deli, "Ada")
   puts "Welcome, #{name}. You are number #{number} in line."
 end
 
